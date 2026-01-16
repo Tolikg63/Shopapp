d744e7a04a68da4a21accbe2d9e8d0f06d190f72
Tolik <ttserunyan@inbox.ru>
2026-01-16 22:39:26 +0400

    added registrarion

--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from . import views
 
 app_name = 'myauth'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='myauth/login.html', redirect_authenticated_user=True), name='login'),
+    path(
+        'login/', 
+        LoginView.as_view(template_name='myauth/login.html', 
+        redirect_authenticated_user=True), 
+        name='login'),
+        # path('logout/', views.MyLogoutView.as_view(), name='logout'),
+        path('logout/', views.logout_view, name='logout'),
+        path('about-me/', views.AboutMeView.as_view(), name='about-me'),
+        path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,5 +1,39 @@
-from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 
+def logout_view(request):
+    logout(request)
+    return redirect('myauth:login')
 
+# Պետք է ստուգել
+# class MyLogoutView(LogoutView):
+#     next_page = reverse_lazy('myauth:login')
+
+
+class AboutMeView(TemplateView):
+    template_name = 'myauth/about-me.html'
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'myauth/register.html'
+    success_url = 'myauth/about-me.html'
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+        )
+        login(request=self.request, user=user)
+        return response
+    
