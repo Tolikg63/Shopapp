cd434a6f342d17a61116b27d7cd3d528c3b20873
Tolik <ttserunyan@inbox.ru>
2026-01-16 16:25:09 +0400

    added authentication with LoginView

---
+++ mysite/myauth/views.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+
+
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shopapp.apps.ShopappConfig',
     'requestdataapp.apps.RequestdataappConfig',
+    'myauth.apps.MyauthConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin/'
