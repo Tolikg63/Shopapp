fa345b0980260fecac9a009f261f47d5d05cede9
Tolik <ttserunyan@inbox.ru>
2026-01-16 23:34:15 +0400

    added Profile model and pair it with User model

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 def logout_view(request):
@@ -22,11 +23,11 @@ class AboutMeView(TemplateView):
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'myauth/register.html'
-    success_url = 'myauth/about-me.html'
+    success_url = reverse_lazy('myauth:about-me')
     
     def form_valid(self, form):
         response = super().form_valid(form)
-
+        Profile.objects.create(user=self.object)
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(
