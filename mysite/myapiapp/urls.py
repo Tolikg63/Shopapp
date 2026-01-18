c2e191798784e470732322f825e1204cf5db7392
Tolik <ttserunyan@inbox.ru>
2026-01-18 19:47:44 +0400

    added serializer

--- mysite/myapiapp/urls.py
+++ mysite/myapiapp/urls.py
@@ -5,4 +5,7 @@ app_name = 'myapiapp'
 
 urlpatterns = [
     path('hello/', views.hello_world_view, name='hello'),
+    path('groups/', views.group_view, name='group'),
+    path('users/', views.UserListView.as_view(), name='user'),
+    path('groups-list/', views.GroupListView.as_view(), name='group-list'),
 ]
--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,8 +1,32 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
-from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import GroupSerializer
 
 
 @api_view()
 def hello_world_view(request):
-    return Response({"message": "Hello World"})
+    return Response({"message": "Hi I'm here"})
+
+
+@api_view()
+def group_view(request):
+    groups = Group.objects.all()
+    data = [group.name for group in groups]
+    return Response({"group": data})
+
+
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        data = [user.username for user in users]
+        return Response({"User": data})
+    
+
+class GroupListView(APIView):
+    def get(self, request):
+        groups = Group.objects.all()
+        serialized = GroupSerializer(groups, many=True)
+        return Response({'message': serialized.data})
