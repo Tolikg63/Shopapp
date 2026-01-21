08a15abf04b599e9313a3d2e5bea54c0c3c630f6
Tolik <ttserunyan@inbox.ru>
2026-01-21 23:16:47 +0400

    rest framework configured

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('myauth:about-me')
 
-REST_FRAMEWORKS = {
+REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
